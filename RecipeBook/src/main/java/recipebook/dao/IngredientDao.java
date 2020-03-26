fd068de01417a60f91f5b52b841c5a4436f849e9
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-26 20:32:02 +0200

    Update IngredientDao interface

---
+++ RecipeBook/src/main/java/recipebook/dao/IngredientDao.java
@@ -0,0 +1,13 @@
+package recipebook.dao;
+
+import java.util.List;
+import recipebook.domain.Ingredient;
+
+public interface IngredientDao {
+
+    Ingredient create(Ingredient ingredient);
+
+    List<Ingredient> getAll();
+
+    Ingredient getByName(String name);
+}
