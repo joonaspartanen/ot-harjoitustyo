71866191dd0f325928d2cf004a2e01f4ea0813b0
Joonas Partanen <partanen.joonas@gmail.com>
2020-03-29 17:26:38 +0300

    Remove commented code

--- RecipeBook/src/main/java/recipebook/Main.java
+++ RecipeBook/src/main/java/recipebook/Main.java
@@ -1,43 +1,12 @@
 package recipebook;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
-import recipebook.dao.ArrayListIngredientDao;
-import recipebook.dao.ArrayListRecipeDao;
-import recipebook.dao.IngredientDao;
-import recipebook.dao.RecipeDao;
-import recipebook.domain.Ingredient;
-import recipebook.domain.Recipe;
 import recipebook.ui.TextUi;
 
 public class Main {
 
     public static void main(String[] args) {
-        /* System.out.println("Starting app");
-        System.out.println("");
-        
-        RecipeDao recipeDao = new ArrayListRecipeDao();
-        IngredientDao ingDao = new ArrayListIngredientDao();
-        
-        ingDao.create(new Ingredient("Chicken"));
-        ingDao.create(new Ingredient("Butter"));
-        ingDao.create(new Ingredient("Onion"));
-        ingDao.create(new Ingredient("Garlic"));
-        
-        HashMap<Ingredient, Integer> ingredients = new HashMap<>();
-        ingredients.put(ingDao.getByName("Chicken"), 400);
-        ingredients.put(ingDao.getByName("Butter"), 50);
-        ingredients.put(ingDao.getByName("Garlic"), 5);
-        ingredients.put(ingDao.getByName("Onion"), 50);
-        
-        Recipe testRecipe = recipeDao.create(new Recipe(1, "Butter Chicken", ingredients, 40, "Cook until ready"));
-        
-        List<Ingredient> availableIngredients = ingDao.getAll();
-        System.out.println(testRecipe);
-        System.out.println("");
-        System.out.println(availableIngredients);*/
-        
+
         Scanner reader = new Scanner(System.in);
         TextUi tui = new TextUi(reader);
         tui.start();
