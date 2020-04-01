87ab7be7e6392d3d00b8bb1477f692a00104385a
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-01 18:11:29 +0300

    Update tests for ArrayListRecipeDao

--- RecipeBook/src/test/java/recipebook/dao/ArrayListRecipeDaoTest.java
+++ RecipeBook/src/test/java/recipebook/dao/ArrayListRecipeDaoTest.java
@@ -1,51 +1,53 @@
 package recipebook.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import static org.hamcrest.CoreMatchers.is;
-import org.junit.Test;
-import static org.junit.Assert.*;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import recipebook.TestHelper;
 import recipebook.domain.Ingredient;
 import recipebook.domain.Recipe;
 
 public class ArrayListRecipeDaoTest {
 
     private RecipeDao recipeDao;
+    private TestHelper helper;
 
     @Before
     public void setUp() {
-        recipeDao = new ArrayListRecipeDao();
+        recipeDao = new ArrayListRecipeDao(new ArrayListIngredientDao());
+        helper = new TestHelper();
     }
 
     @Test
     public void createdRecipeGetsRightId() {
         initializeRecipeBook(3);
-        Recipe recipe = recipeDao.create(createTestRecipe("Salmon soup"));
+        Recipe recipe = recipeDao.create(helper.createTestRecipe("Salmon soup"));
         assertThat(recipe.getId(), is(4));
+    }
+
+    @Test
+    public void recipeIsNotAddedIfItAlreadyExists() {
+        Recipe recipe = helper.createTestRecipe("Salmon soup");
+        recipeDao.create(recipe);
+        assertThat(recipeDao.getAll().size(), is(1));
+        recipeDao.create(recipe);
+        assertThat(recipeDao.getAll().size(), is(1));
     }
 
     public void initializeRecipeBook(int numberOfRecipes) {
         List<Recipe> recipes = new ArrayList<>();
-        Map<Ingredient, Integer> ingredients = createTestIngredientList();
+        Map<Ingredient, Integer> ingredients = helper.createTestIngredientList();
 
         for (int i = 0; i < numberOfRecipes; i++) {
             recipes.add(new Recipe(i, "Recipe " + i, ingredients, 40, "Cook until done"));
         }
         recipeDao.setRecipes(recipes);
-    }
-
-    public Recipe createTestRecipe(String name) {
-        Map<Ingredient, Integer> ingredients = createTestIngredientList();
-        return new Recipe(name, ingredients, 30, "Cook until done");
-    }
-
-    public Map<Ingredient, Integer> createTestIngredientList() {
-        Map<Ingredient, Integer> ingredients = new HashMap<>();
-        ingredients.put(new Ingredient("Chicken"), 300);
-        ingredients.put(new Ingredient("Butter"), 40);
-        return ingredients;
     }
 }
