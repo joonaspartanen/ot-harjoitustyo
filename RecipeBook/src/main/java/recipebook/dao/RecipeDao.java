a25c95b04d3dcd4590d3829ffdbc210783da854c
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-01 18:16:15 +0300

    Add new dao methods

--- RecipeBook/src/main/java/recipebook/dao/RecipeDao.java
+++ RecipeBook/src/main/java/recipebook/dao/RecipeDao.java
@@ -1,6 +1,7 @@
 package recipebook.dao;
 
 import java.util.List;
+
 import recipebook.domain.Recipe;
 
 public interface RecipeDao {
@@ -10,6 +11,10 @@ public interface RecipeDao {
     List<Recipe> getAll();
 
     List<Recipe> getByName(String name);
+
+    Recipe getById(int id);
+
+    List<Recipe> getByIngredient(String name);
 
     boolean delete(Recipe recipe);
 
