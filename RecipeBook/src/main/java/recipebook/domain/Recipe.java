359a6305072a1ae94d79919b07b79c15809f8209
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-01 18:18:26 +0300

    Add getIngredients method

--- RecipeBook/src/main/java/recipebook/domain/Recipe.java
+++ RecipeBook/src/main/java/recipebook/domain/Recipe.java
@@ -49,4 +49,8 @@ public class Recipe {
     public void setId(int id) {
         this.id = id;
     }
+
+    public Map<Ingredient, Integer> getIngredients() {
+        return ingredients;
+    }
 }
