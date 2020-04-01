8f8f1680adbe7985e3e7890f3f060c3ffa305e12
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-01 18:18:51 +0300

    Add findByIngredient method

--- RecipeBook/src/main/java/recipebook/domain/RecipeService.java
+++ RecipeBook/src/main/java/recipebook/domain/RecipeService.java
@@ -3,24 +3,27 @@ package recipebook.domain;
 
 import java.util.List;
 import java.util.Map;
+
 import recipebook.dao.RecipeDao;
 
 public class RecipeService {
-    
+
     private RecipeDao recipeDao;
 
     public RecipeService(RecipeDao recipeDao) {
         this.recipeDao = recipeDao;
     }
-    
+
     public List<Recipe> listAll() {
         return recipeDao.getAll();
     }
-    
+
     public Recipe addRecipe(String name, Map<Ingredient, Integer> ingredients, int time, String instructions) {
         Recipe recipe = new Recipe(name, ingredients, time, instructions);
         return recipeDao.create(recipe);
     }
-    
-    
+
+    public List<Recipe> findByIngredient(String name) {
+        return recipeDao.getByIngredient(name);
+    }
 }
