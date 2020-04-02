08ad20f336b0a883075c5d2eb1f5734586e3994e
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-02 20:21:09 +0300

    Remove commented code

--- RecipeBook/src/main/java/recipebook/Main.java
+++ RecipeBook/src/main/java/recipebook/Main.java
@@ -7,10 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         GraphicUi.main(args);
-
-        /*
-         * Scanner reader = new Scanner(System.in); TextUi tui = new TextUi(reader);
-         * tui.start();
-         */
+    
     }
 }
