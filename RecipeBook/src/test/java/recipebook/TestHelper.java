c74917e4eefa3cfa82722355a16a091fffb59a6c
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-09 17:03:25 +0300

    Create new helper methods for tests

--- RecipeBook/src/test/java/recipebook/TestHelper.java
+++ RecipeBook/src/test/java/recipebook/TestHelper.java
@@ -1,8 +1,11 @@
 package recipebook;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import recipebook.dao.RecipeDao;
 import recipebook.domain.Ingredient;
 import recipebook.domain.Recipe;
 
@@ -10,14 +13,41 @@ public class TestHelper {
 
   public Map<Ingredient, Integer> createTestIngredientList() {
     Map<Ingredient, Integer> ingredients = new HashMap<>();
-    ingredients.put(new Ingredient("Chicken"), 300);
-    ingredients.put(new Ingredient("Butter"), 40);
+    ingredients.put(new Ingredient("ingredient1"), 300);
+    ingredients.put(new Ingredient("ingredient2"), 40);
+    return ingredients;
+  }
+
+  public Map<Ingredient, Integer> createTestIngredientListWithNames(String... ingredientNames) {
+    Map<Ingredient, Integer> ingredients = new HashMap<>();
+    for (String name : ingredientNames) {
+      ingredients.put(new Ingredient(name), 300);
+    }
     return ingredients;
   }
 
   public Recipe createTestRecipe(String name) {
     Map<Ingredient, Integer> ingredients = createTestIngredientList();
     return new Recipe(name, ingredients, 30, "Cook until done");
+  }
+
+  public Recipe createTestRecipeWithIngredients(String recipeName, String... ingredientNames) {
+    Map<Ingredient, Integer> ingredients = new HashMap<>();
+    for (String name : ingredientNames) {
+      ingredients.put(new Ingredient(name), 100);
+    }
+    Recipe recipe = new Recipe(recipeName, ingredients, 20, "Cook well");
+    return recipe;
+  }
+
+  public void initializeRecipeBook(int numberOfRecipes, RecipeDao recipeDao) {
+    List<Recipe> recipes = new ArrayList<>();
+    Map<Ingredient, Integer> ingredients = createTestIngredientList();
+
+    for (int i = 0; i < numberOfRecipes; i++) {
+      recipes.add(new Recipe(i, "Recipe " + i, ingredients, 40, "Cook until done"));
+    }
+    recipeDao.setRecipes(recipes);
   }
 
 }
