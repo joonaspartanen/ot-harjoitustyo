695f27e283d25ad2cef1b3e5b82855e02f3783a5
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-09 17:01:37 +0300

    Create tests for ArrayListRecipeDao

--- RecipeBook/src/test/java/recipebook/dao/ArrayListRecipeDaoTest.java
+++ RecipeBook/src/test/java/recipebook/dao/ArrayListRecipeDaoTest.java
@@ -3,15 +3,10 @@ package recipebook.dao;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import recipebook.TestHelper;
-import recipebook.domain.Ingredient;
 import recipebook.domain.Recipe;
 
 public class ArrayListRecipeDaoTest {
@@ -27,7 +22,7 @@ public class ArrayListRecipeDaoTest {
 
     @Test
     public void createdRecipeGetsRightId() {
-        initializeRecipeBook(3);
+        helper.initializeRecipeBook(3, recipeDao);
         Recipe recipe = recipeDao.create(helper.createTestRecipe("Salmon soup"));
         assertThat(recipe.getId(), is(4));
     }
@@ -41,13 +36,5 @@ public class ArrayListRecipeDaoTest {
         assertThat(recipeDao.getAll().size(), is(1));
     }
 
-    public void initializeRecipeBook(int numberOfRecipes) {
-        List<Recipe> recipes = new ArrayList<>();
-        Map<Ingredient, Integer> ingredients = helper.createTestIngredientList();
-
-        for (int i = 0; i < numberOfRecipes; i++) {
-            recipes.add(new Recipe(i, "Recipe " + i, ingredients, 40, "Cook until done"));
-        }
-        recipeDao.setRecipes(recipes);
-    }
+   
 }
