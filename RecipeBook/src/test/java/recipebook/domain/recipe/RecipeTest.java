a897471142fc0a1d498045b4073ed9f13df9b4b2
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-13 17:54:20 +0300

    Add tests for equals method

--- RecipeBook/src/test/java/recipebook/domain/recipe/RecipeTest.java
+++ RecipeBook/src/test/java/recipebook/domain/recipe/RecipeTest.java
@@ -1,21 +1,34 @@
-package recipebook.domain;
+package recipebook.domain.recipe;
+
+import recipebook.domain.recipe.Recipe;
+import recipebook.domain.ingredient.Ingredient;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-import java.util.HashMap;
+import recipebook.TestHelper;
 
 public class RecipeTest {
 
     Recipe butterChicken;
-
+    HashMap<Ingredient, Integer> ingredients;
+    TestHelper helper;
+    
     @Before
     public void setUp() {
+        helper = new TestHelper();
         Ingredient chicken = new Ingredient("chicken");
         Ingredient butter = new Ingredient("butter");
         Ingredient onion = new Ingredient("onion");
-        HashMap<Ingredient, Integer> ingredients = new HashMap<>();
+        ingredients = new HashMap<>();
         ingredients.put(chicken, 400);
         ingredients.put(butter, 50);
         ingredients.put(onion, 50);
@@ -25,7 +38,53 @@ public class RecipeTest {
     @Test
     public void toStringFormatsRecipeProperly() {
         String result = butterChicken.toString();
-        assertEquals("Butter Chicken\n\nIngredients:\n50 g butter\n400 g chicken\n50 g onion\n\nCooking time: 40 min\n\nInstructions:\nCook until ready\n", result);
+        assertEquals(
+                "Butter Chicken\n\nIngredients:\n50 g butter\n400 g chicken\n50 g onion\n\nCooking time: 40 min\n\nInstructions:\nCook until ready\n",
+                result);
+    }
+
+    @Test
+    public void equalsReturnsTrueIfRecipesEqual() {
+        Recipe otherRecipe = new Recipe(1, "Butter Chicken", ingredients, 40, "Cook until ready");
+        assertThat(butterChicken, is(equalTo(otherRecipe)));
+    }
+
+
+    @Test
+    public void equalsReturnsFalseIfRecipeIdsDiffer() {
+        Recipe otherRecipe = new Recipe(2, "Butter Chicken", ingredients, 40, "Cook until ready");
+        assertThat(butterChicken, is(not(equalTo(otherRecipe))));
+    }
+
+    @Test
+    public void equalsReturnsFalseIfRecipeNamesDiffer() {
+        Recipe otherRecipe = new Recipe(1, "Better Chicken", ingredients, 40, "Cook until ready");
+        assertThat(butterChicken, is(not(equalTo(otherRecipe))));
+    }
+
+    @Test
+    public void equalsReturnsFalseIfRecipeTimesDiffer() {
+        Recipe otherRecipe = new Recipe(1, "Butter Chicken", ingredients, 120, "Cook until ready");
+        assertThat(butterChicken, is(not(equalTo(otherRecipe))));
+    }
+
+    @Test
+    public void equalsReturnsFalseIfRecipeInstructionsDiffer() {
+        Recipe otherRecipe = new Recipe(1, "Butter Chicken", ingredients, 40, "Cook until well done");
+        assertThat(butterChicken, is(not(equalTo(otherRecipe))));
+    }
+
+    @Test
+    public void equalsReturnsFalseIfRecipeIngredientsDiffer() {
+        Map<Ingredient, Integer> otherIngredients = helper.createTestIngredientListWithNames("chicken", "butter", "cream");
+        Recipe otherRecipe = new Recipe(1, "Butter Chicken", otherIngredients, 40, "Cook until ready");
+        assertThat(butterChicken, is(not(equalTo(otherRecipe))));
+    }
+
+    @Test
+    public void equalsReturnsFalseIfComparedWithOtherClass() {
+        Ingredient chicken = new Ingredient("chicken", "g");
+        assertThat(butterChicken, is(not(equalTo(chicken))));
     }
 
 }
