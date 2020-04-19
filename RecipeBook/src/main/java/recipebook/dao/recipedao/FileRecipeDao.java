64f9c5ea6d95baa35a2f98d9ebae487b81fc5b98
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-19 15:02:14 +0300

    Use wildcard imports

--- RecipeBook/src/main/java/recipebook/dao/recipedao/FileRecipeDao.java
+++ RecipeBook/src/main/java/recipebook/dao/recipedao/FileRecipeDao.java
@@ -1,17 +1,8 @@
 package recipebook.dao.recipedao;
 
 import recipebook.dao.ingredientdao.IngredientDao;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import recipebook.domain.ingredient.Ingredient;
