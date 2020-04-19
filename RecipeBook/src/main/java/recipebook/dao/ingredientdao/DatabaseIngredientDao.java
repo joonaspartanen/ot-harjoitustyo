5157b17e9d00740465b8cae7b2d571c5e27852c3
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-19 15:01:19 +0300

    Implement query builder and resultSetMapper

--- RecipeBook/src/main/java/recipebook/dao/ingredientdao/DatabaseIngredientDao.java
+++ RecipeBook/src/main/java/recipebook/dao/ingredientdao/DatabaseIngredientDao.java
@@ -1,33 +1,32 @@
 package recipebook.dao.ingredientdao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.*;
+import java.util.Collections;
 import java.util.List;
-import recipebook.dao.DaoHelper;
 
+import recipebook.dao.DaoHelper;
+import recipebook.dao.QueryBuilder;
+import recipebook.dao.ResultSetMapper;
 import recipebook.domain.ingredient.Ingredient;
 
 public class DatabaseIngredientDao implements IngredientDao {
 
     private Connection connection;
     private DaoHelper daoHelper;
+    private ResultSetMapper mapper;
 
     public DatabaseIngredientDao(Connection connection) {
         this.connection = connection;
         daoHelper = new DaoHelper();
+        mapper = new ResultSetMapper();
     }
 
     @Override
     public Ingredient create(Ingredient ingredient) {
-        String createIngredientQuery = "INSERT INTO Ingredients (name, unit) VALUES (?, ?);";
+        String createIngredientQuery = QueryBuilder.generateInsertIngredientQuery();
 
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(createIngredientQuery,
-                    Statement.RETURN_GENERATED_KEYS);
+        try (PreparedStatement pstmt = connection.prepareStatement(createIngredientQuery,
+                Statement.RETURN_GENERATED_KEYS)) {
             pstmt.setString(1, ingredient.getName());
             pstmt.setString(2, ingredient.getUnit());
             pstmt.executeUpdate();
@@ -35,9 +34,9 @@ public class DatabaseIngredientDao implements IngredientDao {
             int id = daoHelper.getCreatedItemId(pstmt);
             ingredient.setId(id);
 
-            pstmt.close();
         } catch (SQLException e) {
             System.out.println("Creating ingredient " + ingredient.getName() + " failed.");
+            e.printStackTrace();
         }
 
         return ingredient;
@@ -45,71 +44,49 @@ public class DatabaseIngredientDao implements IngredientDao {
 
     @Override
     public List<Ingredient> getAll() {
-        List<Ingredient> ingredients = new ArrayList<>();
-        String selectAllQuery = "SELECT * FROM Ingredients;";
+        String selectAllQuery = QueryBuilder.generateSelectAllIngredientsQuery();
+        try (PreparedStatement pstmt = connection.prepareStatement(selectAllQuery)) {
+            List<Ingredient> foundIngredients = mapper.extractIngredientList(pstmt);
+            return foundIngredients;
 
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(selectAllQuery);
-            ResultSet resultSet = pstmt.executeQuery();
-
-            while (resultSet.next()) {
-                Ingredient ingredient = createIngredientFromResultSet(resultSet);
-                ingredients.add(ingredient);
-            }
-
-            pstmt.close();
         } catch (SQLException e) {
             System.out.println("Fetching the ingredients from database failed.");
+            e.printStackTrace();
         }
-        return ingredients;
-    }
 
-    private Ingredient createIngredientFromResultSet(ResultSet resultSet) throws SQLException {
-        int id = resultSet.getInt("id");
-        String name = resultSet.getString("name");
-        String unit = resultSet.getString("unit");
-        Ingredient ingredient = new Ingredient(id, name, unit);
-        return ingredient;
+        return Collections.emptyList();
     }
 
     @Override
     public List<Ingredient> getByName(String name) {
-        List<Ingredient> ingredients = new ArrayList<>();
-        String selectWhereNameQuery = "SELECT * FROM Ingredients WHERE name = ?;";
-
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(selectWhereNameQuery);
+        String selectWhereNameQuery = QueryBuilder.generateSelectAllIngredientsByNameQuery();
+        try (PreparedStatement pstmt = connection.prepareStatement(selectWhereNameQuery)) {
             pstmt.setString(1, name);
-            ResultSet resultSet = pstmt.executeQuery();
 
-            while (resultSet.next()) {
-                Ingredient ingredient = createIngredientFromResultSet(resultSet);
-                ingredients.add(ingredient);
-            }
+            List<Ingredient> foundIngredients = mapper.extractIngredientList(pstmt);
+            return foundIngredients;
 
-            pstmt.close();
         } catch (SQLException e) {
             System.out.println("Fetching ingredient " + name + " from database failed.");
+            e.printStackTrace();
         }
 
-        return ingredients;
+        return Collections.emptyList();
     }
 
     @Override
     public Ingredient getById(int id) {
         Ingredient ingredient = null;
-        String selectByNameQuery = "SELECT * FROM Ingredients WHERE id = ?;";
+        String selectByNameQuery = QueryBuilder.generateSelectAllIngredientsByIdQuery();
 
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(selectByNameQuery);
+        try (PreparedStatement pstmt = connection.prepareStatement(selectByNameQuery)) {
             pstmt.setInt(1, id);
-            ResultSet resultSet = pstmt.executeQuery();
 
-            ingredient = createIngredientFromResultSet(resultSet);
+            ingredient = mapper.extractSingleIngredient(pstmt);
 
-            pstmt.close();
         } catch (SQLException e) {
             System.out.println("Fetching ingredient with id " + id + " from database failed.");
+            e.printStackTrace();
         }
 
         return ingredient;
@@ -117,20 +94,18 @@ public class DatabaseIngredientDao implements IngredientDao {
 
     public Ingredient getByNameAndUnit(String name, String unit) {
         Ingredient ingredient = null;
-        String selectByNameAndUnitQuery = "SELECT * FROM Ingredients WHERE name = ? AND unit = ?;";
+        String selectByNameAndUnitQuery = QueryBuilder.generateSelectAllIngredientsByNameAndUnitQuery();
 
-        try {
-            PreparedStatement pstmt = connection.prepareStatement(selectByNameAndUnitQuery);
+        try (PreparedStatement pstmt = connection.prepareStatement(selectByNameAndUnitQuery)) {
             pstmt.setString(1, name);
             pstmt.setString(2, unit);
-            ResultSet resultSet = pstmt.executeQuery();
 
-            ingredient = createIngredientFromResultSet(resultSet);
+            ingredient = mapper.extractSingleIngredient(pstmt);
 
-            pstmt.close();
         } catch (SQLException e) {
             System.out
                     .println("Fetching ingredient with name " + name + " and unit " + unit + " from database failed.");
+            e.printStackTrace();
         }
 
         return ingredient;
