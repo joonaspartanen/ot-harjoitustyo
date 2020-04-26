a728c53e0edece3a4f2fe85cadf52e9cc4849a7b
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-26 17:22:33 +0300

    Add user to recipe

--- RecipeBook/src/main/java/recipebook/domain/recipe/Recipe.java
+++ RecipeBook/src/main/java/recipebook/domain/recipe/Recipe.java
@@ -1,6 +1,8 @@
 package recipebook.domain.recipe;
 
 import recipebook.domain.ingredient.Ingredient;
+import recipebook.domain.user.User;
+
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -11,6 +13,7 @@ public class Recipe {
     private Map<Ingredient, Integer> ingredients;
     private int time;
     private String instructions;
+    private User author;
 
     public Recipe(String name, Map<Ingredient, Integer> ingredients, int time, String instructions) {
         this.name = name;
@@ -24,6 +27,17 @@ public class Recipe {
         this.id = id;
     }
 
+    public Recipe(String name, Map<Ingredient, Integer> ingredients, int time, String instructions, User author) {
+        this(name, ingredients, time, instructions);
+        this.author = author;
+    }
+
+    public Recipe(int id, String name, Map<Ingredient, Integer> ingredients, int time, String instructions,
+            User author) {
+        this(id, name, ingredients, time, instructions);
+        this.author = author;
+    }
+
     public String stringifyIngredients() {
         String result = ingredients.entrySet().stream().sorted(Map.Entry.comparingByKey())
                 .map(i -> i.getValue() + " " + i.getKey().getUnit() + " " + i.getKey() + "\n")
@@ -34,7 +48,7 @@ public class Recipe {
     @Override
     public String toString() {
         return name + "\n\n" + "Ingredients:" + "\n" + stringifyIngredients() + "\n" + "Cooking time: " + time + " min"
-                + "\n\n" + "Instructions:" + "\n" + instructions + "\n";
+                + "\n\n" + "Instructions:" + "\n" + instructions + "\n\n" + "Recipe created by: " + author + "\n";
     }
 
     public String getName() {
@@ -80,5 +94,9 @@ public class Recipe {
         return this.id == other.getId() && this.name.equals(other.getName())
                 && this.ingredients.equals(other.getIngredients()) && this.time == other.getTime()
                 && this.instructions.equals(other.getInstructions());
+    }
+
+    public int getAuthorId() {
+        return author.getId();
     }
 }
--- RecipeBook/src/main/java/recipebook/domain/recipe/RecipeService.java
+++ RecipeBook/src/main/java/recipebook/domain/recipe/RecipeService.java
@@ -2,39 +2,89 @@
 package recipebook.domain.recipe;
 
 import recipebook.domain.ingredient.Ingredient;
+import recipebook.domain.user.User;
+import recipebook.domain.user.UserService;
+
 import java.util.List;
 import java.util.Map;
 
 import recipebook.dao.recipedao.RecipeDao;
 
+/**
+ * Contains the business logic for handling Recipes objects. Dependencies are
+ * injected in the constructor.
+ */
 public class RecipeService {
 
     private RecipeDao recipeDao;
+    private UserService userService;
 
-    public RecipeService(RecipeDao recipeDao) {
+    public RecipeService(RecipeDao recipeDao, UserService userService) {
         this.recipeDao = recipeDao;
+        this.userService = userService;
     }
 
+    /**
+     * Method to fetch all recipes stored in the current data store.
+     * 
+     * @return List of recipes.
+     */
     public List<Recipe> listAll() {
         return recipeDao.getAll();
     }
 
+    /**
+     * Method to create a new recipe and store using the dao dependency. Parameters
+     * self-explanatory. Gets the recipe author by the UserService.
+     * 
+     * @param name
+     * @param ingredients
+     * @param time
+     * @param instructions
+     * @return The newly created Recipe object.
+     */
     public Recipe createRecipe(String name, Map<Ingredient, Integer> ingredients, int time, String instructions) {
-        Recipe recipe = new Recipe(name, ingredients, time, instructions);
+        User currentUser = userService.getCurrentUser();
+        Recipe recipe = new Recipe(name, ingredients, time, instructions, currentUser);
         return recipeDao.create(recipe);
     }
 
+    /**
+     * Method to fetch all recipes that contain certain ingredient.
+     * 
+     * @param name Name of the ingredient that matching recipes should contain.
+     * @return List of recipes.
+     */
     public List<Recipe> findByIngredient(String name) {
         return recipeDao.getByIngredient(name);
     }
 
+    /**
+     * Method to delete a recipe from the database or file. Checks if the recipe
+     * exists and if the current user is its author.
+     * 
+     * @param recipeId Id of the recipe to delete.
+     * @return true if the recipe is deleted successfully; false if the operation
+     *         fails or is denied.
+     */
     public boolean deleteRecipeById(int recipeId) {
         Recipe recipe = recipeDao.getById(recipeId);
         if (recipe == null) {
             return false;
         }
-        recipeDao.delete(recipe);
-        return true;
+
+        User currentUser = userService.getCurrentUser();
+
+        if (userAllowedToDeleteRecipe(currentUser, recipe)) {
+            recipeDao.delete(recipe);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean userAllowedToDeleteRecipe(User user, Recipe recipe) {
+        return user.getId() == recipe.getAuthorId();
     }
 
 }
