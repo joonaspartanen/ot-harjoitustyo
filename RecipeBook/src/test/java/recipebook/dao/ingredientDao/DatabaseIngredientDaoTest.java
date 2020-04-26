0e64524670dc3b8bcb1f68fe2a258d4188959dbe
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-26 18:02:37 +0300

    Rename variable for clarity

--- RecipeBook/src/test/java/recipebook/dao/ingredientDao/DatabaseIngredientDaoTest.java
+++ RecipeBook/src/test/java/recipebook/dao/ingredientDao/DatabaseIngredientDaoTest.java
@@ -22,7 +22,7 @@ public class DatabaseIngredientDaoTest {
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
 
-    IngredientDao ingDao;
+    IngredientDao ingredientDao;
     DataStoreConnector connector;
     Connection connection;
 
@@ -30,7 +30,7 @@ public class DatabaseIngredientDaoTest {
     public void setUp() throws DatabaseException {
         connector = new DatabaseConnector(testFolder.getRoot().toString() + "/");
         connector.initializeDataStore();
-        ingDao = connector.getIngredientDao();
+        ingredientDao = connector.getIngredientDao();
     }
 
     @After
@@ -40,73 +40,73 @@ public class DatabaseIngredientDaoTest {
 
     @Test
     public void createdIngredientHasRightName() {
-        Ingredient ingredient = ingDao.create(new Ingredient("chicken"));
+        Ingredient ingredient = ingredientDao.create(new Ingredient("chicken"));
         assertThat(ingredient.getName(), is(equalTo("chicken")));
     }
 
     @Test
     public void createdIngredientHasRightId() {
-        Ingredient ingredient = ingDao.create(new Ingredient("tomato"));
+        Ingredient ingredient = ingredientDao.create(new Ingredient("tomato"));
         assertThat(ingredient.getId(), is(1));
 
-        ingredient = ingDao.create(new Ingredient("potato"));
+        ingredient = ingredientDao.create(new Ingredient("potato"));
         assertThat(ingredient.getId(), is(2));
     }
 
     @Test
     public void getAllReturnsRightNumberOfIngredients() {
-        ingDao.create(new Ingredient("banana"));
-        ingDao.create(new Ingredient("apple"));
-        ingDao.create(new Ingredient("kiwi"));
+        ingredientDao.create(new Ingredient("banana"));
+        ingredientDao.create(new Ingredient("apple"));
+        ingredientDao.create(new Ingredient("kiwi"));
 
-        List<Ingredient> ingredients = ingDao.getAll();
+        List<Ingredient> ingredients = ingredientDao.getAll();
         
         assertThat(ingredients.size(), is(3));
     }
 
     @Test
     public void getByNameAndUnitReturnsRightIngredientWhenFound() {
-        ingDao.create(new Ingredient("salmon"));
-        ingDao.create(new Ingredient("milk"));
+        ingredientDao.create(new Ingredient("salmon"));
+        ingredientDao.create(new Ingredient("milk"));
 
-        Ingredient ingredient = ingDao.getByNameAndUnit("salmon", "g");
+        Ingredient ingredient = ingredientDao.getByNameAndUnit("salmon", "g");
         
         assertThat(ingredient.getName(), is(equalTo("salmon")));
     }
 
     @Test
     public void getByNameAndUnitReturnsNullIfNotFound() {
-        ingDao.create(new Ingredient("salmon"));
+        ingredientDao.create(new Ingredient("salmon"));
 
-        Ingredient ingredient = ingDao.getByNameAndUnit("chicken", "g");
+        Ingredient ingredient = ingredientDao.getByNameAndUnit("chicken", "g");
         
         assertThat(ingredient, is(nullValue()));
     }
 
     @Test
     public void getByIdReturnsRightIngredient() {
-        ingDao.create(new Ingredient("meat"));
-        ingDao.create(new Ingredient("fish"));
+        ingredientDao.create(new Ingredient("meat"));
+        ingredientDao.create(new Ingredient("fish"));
         
-        Ingredient ingredient = ingDao.getById(1);
+        Ingredient ingredient = ingredientDao.getById(1);
         assertThat(ingredient.getName(), is(equalTo("meat")));
         
-        ingredient = ingDao.getById(2);
+        ingredient = ingredientDao.getById(2);
         assertThat(ingredient.getName(), is(equalTo("fish")));
     }
 
     @Test
     public void getByIdReturnsNullIfNotFound() {
-        ingDao.create(new Ingredient("salmon"));
-        Ingredient ingredient = ingDao.getById(2);
+        ingredientDao.create(new Ingredient("salmon"));
+        Ingredient ingredient = ingredientDao.getById(2);
         assertThat(ingredient, is(nullValue()));
     }
 
     @Test
     public void rightIngredientsAreLoadedWhenObjectInstantiated() {
-        ingDao.create(new Ingredient("salmon"));
-        ingDao.create(new Ingredient("milk"));
-        ingDao.create(new Ingredient("chicken"));
+        ingredientDao.create(new Ingredient("salmon"));
+        ingredientDao.create(new Ingredient("milk"));
+        ingredientDao.create(new Ingredient("chicken"));
         IngredientDao newIngDao = connector.getIngredientDao();
         List<Ingredient> ingredients = newIngDao.getAll();
         List<String> ingredientNames = ingredients.stream().map(i -> i.getName()).collect(Collectors.toList());
