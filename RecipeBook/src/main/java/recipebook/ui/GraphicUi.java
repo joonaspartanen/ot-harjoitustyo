224005e8da3f386c317cb929a0bf90359ddf4df5
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-26 17:22:49 +0300

    Update graphic UI

--- RecipeBook/src/main/java/recipebook/ui/GraphicUi.java
+++ RecipeBook/src/main/java/recipebook/ui/GraphicUi.java
@@ -11,32 +11,44 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import recipebook.dao.DataStoreConnector;
-import recipebook.dao.DatabaseConnector;
-import recipebook.dao.FileConnector;
+import recipebook.dao.*;
 import recipebook.dao.ingredientdao.*;
 import recipebook.dao.recipedao.*;
+import recipebook.dao.userdao.UserDao;
+import recipebook.dao.userdao.UserNotFoundException;
 import recipebook.domain.ingredient.Ingredient;
 import recipebook.domain.ingredient.IngredientService;
 import recipebook.domain.recipe.Recipe;
 import recipebook.domain.recipe.RecipeService;
+import recipebook.domain.user.BadUsernameException;
+import recipebook.domain.user.User;
+import recipebook.domain.user.UserService;
 
 public class GraphicUi extends Application {
 
     private RecipeService recipeService;
     private IngredientService ingredientService;
-    private IngredientDao ingredientDao;
-    private RecipeDao recipeDao;
+    private UserService userService;
+    DataStoreConnector connector;
     Properties properties;
+
+    VBox mainContainer;
+    BorderPane titleWrapper;
+    BorderPane userControlView;
+    TabPane tabPane;
     private ListView<Recipe> recipeList;
+    Label currentUserLabel;
+    Button logoutButton;
+    Alert alert;
+
     private Insets PADDINGBOTTOM10 = new Insets(0, 0, 10, 0);
     private Insets PADDING25 = new Insets(25, 25, 25, 25);
-    DataStoreConnector connector;
-    private DatabaseConnector databaseConnector;
 
     @Override
     public void init() {
@@ -57,19 +69,26 @@ public class GraphicUi extends Application {
             connector = new FileConnector(dataStoreLocation);
         }
 
-        connector.initializeDataStore();
-        ingredientDao = connector.getIngredientDao();
-        recipeDao = connector.getRecipeDao();
+        try {
+            connector.initializeDataStore();
+        } catch (DatabaseException e) {
+            showAlert("Database error", "Error!", e.getMessage(), AlertType.ERROR);
+        }
 
+        IngredientDao ingredientDao = connector.getIngredientDao();
+        RecipeDao recipeDao = connector.getRecipeDao();
+        UserDao userDao = connector.getUserDao();
+
+        userService = new UserService(userDao);
         ingredientService = new IngredientService(ingredientDao);
-        recipeService = new RecipeService(recipeDao);
+        recipeService = new RecipeService(recipeDao, userService);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Java Recipe Book");
 
-        TabPane tabPane = new TabPane();
+        tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
 
         Tab allRecipesTab = initiateAllRecipesTab();
@@ -82,10 +101,136 @@ public class GraphicUi extends Application {
         tabPane.getTabs().add(searchRecipeTab);
         tabPane.getTabs().add(recipeBookTab);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(tabPane);
-        primaryStage.setScene(new Scene(root, 1280, 800));
+        StackPane tabs = new StackPane();
+        tabs.getChildren().add(tabPane);
+
+        alert = new Alert(AlertType.NONE);
+
+        mainContainer = new VBox();
+
+        titleWrapper = new BorderPane();
+        titleWrapper.setPadding(PADDING25);
+
+        Label appTitle = new Label("Welcome to Java Recipe Book");
+        appTitle.setFont(new Font(30));
+
+        HBox currentUserWrapper = new HBox();
+
+        currentUserLabel = new Label("No user logged in");
+        logoutButton = new Button("Logout");
+        logoutButton.setVisible(false);
+
+        logoutButton.setOnAction(e -> {
+            userService.logout();
+            currentUserLabel.setText("");
+            logoutButton.setVisible(false);
+            showLogoutView();
+        });
+
+        currentUserWrapper.setSpacing(10);
+        currentUserWrapper.getChildren().addAll(currentUserLabel, logoutButton);
+        currentUserWrapper.setAlignment(Pos.CENTER_LEFT);
+
+        titleWrapper.setLeft(appTitle);
+        titleWrapper.setRight(currentUserWrapper);
+
+        mainContainer.getChildren().add(titleWrapper);
+
+        if (userService.userNotLoggedIn()) {
+            showUserControlView();
+        }
+
+        primaryStage.setScene(new Scene(mainContainer, 1600, 1000));
         primaryStage.show();
+    }
+
+    private void showUserControlView() {
+        userControlView = initiateUserControlView();
+        mainContainer.getChildren().add(userControlView);
+    }
+
+    private void showLoggedInView() {
+        mainContainer.getChildren().remove(userControlView);
+        mainContainer.getChildren().add(tabPane);
+    }
+
+    private void showLogoutView() {
+        mainContainer.getChildren().remove(tabPane);
+        showUserControlView();
+    }
+
+    private void showAlert(String title, String header, String message, AlertType type) {
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(message);
+        alert.setAlertType(type);
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        alert.show();
+    }
+
+    private BorderPane initiateUserControlView() {
+        BorderPane userControlView = new BorderPane();
+
+        VBox userControlWrapper = new VBox();
+
+        HBox loginWrapper = new HBox();
+        Label loginInputLabel = new Label("Login: ");
+        TextField loginInputField = new TextField();
+        Button loginButton = new Button("Login");
+
+        loginWrapper.getChildren().addAll(loginInputLabel, loginInputField, loginButton);
+        loginWrapper.setSpacing(10);
+        loginWrapper.setPadding(PADDINGBOTTOM10);
+        loginWrapper.setAlignment(Pos.CENTER);
+
+        loginButton.setOnAction(e -> {
+            String username = loginInputField.getText();
+            try {
+                loginInputField.clear();
+                User currentUser = userService.login(username);
+                currentUserLabel.setText(currentUser.getUsername());
+                logoutButton.setVisible(true);
+                showLoggedInView();
+            } catch (UserNotFoundException ex) {
+                showAlert("User Error", "Error!", ex.getMessage(), AlertType.ERROR);
+            }
+
+        });
+
+        HBox newUserWrapper = new HBox();
+        Label newUserLabel = new Label("New user?");
+        TextField newUserInputField = new TextField();
+        Button newUserButton = new Button("Create user account");
+        newUserWrapper.getChildren().addAll(newUserLabel, newUserInputField, newUserButton);
+        newUserWrapper.setSpacing(10);
+        newUserWrapper.setPadding(PADDINGBOTTOM10);
+        newUserWrapper.setAlignment(Pos.CENTER);
+
+        Label userErrorLabel = new Label();
+
+        newUserButton.setOnAction(e -> {
+            String newUserName = newUserInputField.getText();
+            try {
+                newUserInputField.clear();
+                User user = userService.createUser(newUserName);
+                alert.setAlertType(AlertType.INFORMATION);
+                alert.setTitle("New user created");
+                alert.setHeaderText("Welcome!");
+                alert.setContentText("New user " + user.getUsername() + " created.");
+                alert.show();
+            } catch (BadUsernameException ex) {
+                showAlert("User error", "Bad username", ex.getMessage(), AlertType.ERROR);
+            }
+        });
+
+        userControlWrapper.getChildren().addAll(loginWrapper, newUserWrapper, userErrorLabel);
+        userControlWrapper.setPadding(PADDING25);
+        userControlWrapper.setAlignment(Pos.CENTER);
+
+        userControlView.setCenter(userControlWrapper);
+        userControlView.setMinHeight(600);
+
+        return userControlView;
     }
 
     private Tab initiateAllRecipesTab() {
@@ -95,15 +240,12 @@ public class GraphicUi extends Application {
 
         VBox allRecipesWrapper = new VBox();
         Label recipeLabel = new Label();
-        Button updateRecipeListButton = new Button("Update view");
+        recipeLabel.setWrapText(true);
+        recipeLabel.setMinHeight(Region.USE_PREF_SIZE);
         Button showRecipeButton = generateShowRecipeButton(recipeList, recipeLabel);
         Button deleteRecipeButton = new Button("Delete recipe");
 
         refreshRecipes(recipeList);
-
-        updateRecipeListButton.setOnAction(e -> {
-            refreshRecipes(recipeList);
-        });
 
         deleteRecipeButton.setOnAction(e -> {
             if (recipeList.getSelectionModel().getSelectedItem() == null) {
@@ -118,7 +260,7 @@ public class GraphicUi extends Application {
         HBox buttonsWrapper = new HBox();
         buttonsWrapper.setPadding(new Insets(10, 0, 10, 0));
         buttonsWrapper.setSpacing(10);
-        buttonsWrapper.getChildren().addAll(updateRecipeListButton, showRecipeButton, deleteRecipeButton);
+        buttonsWrapper.getChildren().addAll(showRecipeButton, deleteRecipeButton);
 
         allRecipesWrapper.getChildren().add(recipeList);
         allRecipesWrapper.getChildren().add(buttonsWrapper);
@@ -288,6 +430,9 @@ public class GraphicUi extends Application {
         });
 
         Label recipeLabel = new Label();
+        recipeLabel.setWrapText(true);
+        recipeLabel.setMinHeight(Region.USE_PREF_SIZE);
+        
         Button showRecipeButton = generateShowRecipeButton(foundRecipes, recipeLabel);
 
         HBox buttonsWrapper = new HBox();
@@ -367,7 +512,11 @@ public class GraphicUi extends Application {
 
     @Override
     public void stop() {
-        databaseConnector.closeDataStore();
+        try {
+            connector.closeDataStore();
+        } catch (DatabaseException e) {
+            showAlert("Database error", "Error!", e.getMessage(), AlertType.ERROR);
+        }
     }
 
     public static void main(String[] args) {
