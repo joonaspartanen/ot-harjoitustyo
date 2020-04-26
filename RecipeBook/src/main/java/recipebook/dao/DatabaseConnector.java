47ffd6860f1c97568d46ffb99cb5c6d8d85b6575
Joonas Partanen <partanen.joonas@gmail.com>
2020-04-26 17:17:16 +0300

    Update error handling and method names

--- RecipeBook/src/main/java/recipebook/dao/DatabaseConnector.java
+++ RecipeBook/src/main/java/recipebook/dao/DatabaseConnector.java
@@ -7,71 +7,123 @@ import java.sql.SQLException;
 
 import recipebook.dao.ingredientdao.DatabaseIngredientDao;
 import recipebook.dao.recipedao.DatabaseRecipeDao;
+import recipebook.dao.userdao.DatabaseUserDao;
 
 public class DatabaseConnector extends DataStoreConnector {
 
     private Connection connection = null;
 
+    /**
+     * Constructor
+     * 
+     * @param dataStoreLocation Path to the folder where the datastore file is
+     *                          located.
+     */
     public DatabaseConnector(String dataStoreLocation) {
         this.dataStoreLocation = dataStoreLocation;
     }
 
+    /**
+     * Creates the database directory if necessary, connects to the database file,
+     * creates the database tables and dao implementations.
+     */
     @Override
-    public void initializeDataStore() {
+    public void initializeDataStore() throws DatabaseException {
         createDirectoryIfNotExists();
         connectToDatabase();
         createTables();
         createDaoImplementations();
     }
 
-    private void connectToDatabase() {
+    private void connectToDatabase() throws DatabaseException {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:" + dataStoreLocation + "recipes.db");
         } catch (SQLException e) {
-            System.out.println("Connection to database failed.");
-            e.printStackTrace();
+            throw new DatabaseException("Connection to database failed.", e);
         }
     }
 
-    private void createTables() {
+    private void createTables() throws DatabaseException {
+        createUsersTable();
+        createIngredientsTable();
+        createRecipesTable();
+        createRecipesIngredientsTable();
+        createFavoriteRecipesTable();
+    }
+
+    private void createRecipesTable() throws DatabaseException {
         String createRecipesTable = QueryBuilder.generateCreateRecipesTableQuery();
+
+        try (PreparedStatement pstmt = connection.prepareStatement(createRecipesTable)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException("Creating Recipes table failed.", e);
+        }
+    }
+
+    private void createIngredientsTable() throws DatabaseException {
         String createIngredientsTable = QueryBuilder.generateCreateIngredientsTableQuery();
+
+        try (PreparedStatement pstmt = connection.prepareStatement(createIngredientsTable)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException("Creating Ingredients table failed.", e);
+        }
+    }
+
+    private void createRecipesIngredientsTable() throws DatabaseException {
         String createRecipesIngredientsTable = QueryBuilder.generateCreateRecipesIngredientsTableQuery();
 
-        PreparedStatement pstmt;
-
-        try {
-            pstmt = connection.prepareStatement(createRecipesTable);
+        try (PreparedStatement pstmt = connection.prepareStatement(createRecipesIngredientsTable)) {
             pstmt.executeUpdate();
-            pstmt = connection.prepareStatement(createIngredientsTable);
-            pstmt.executeUpdate();
-            pstmt = connection.prepareStatement(createRecipesIngredientsTable);
-            pstmt.executeUpdate();
-            pstmt.close();
         } catch (SQLException e) {
-            System.out.println("Creating database tables failed.");
-            e.printStackTrace();
+            throw new DatabaseException("Creating RecipesIngredients table failed.", e);
+        }
+    }
+
+    private void createUsersTable() throws DatabaseException {
+        String createUsersTable = QueryBuilder.generateCreateUsersTableQuery();
+
+        try (PreparedStatement pstmt = connection.prepareStatement(createUsersTable)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException("Creating Users table failed.", e);
+        }
+    }
+
+    private void createFavoriteRecipesTable() throws DatabaseException {
+        String createFavoriteRecipesTable = QueryBuilder.generateCreateFavoriteRecipesTablesQuery();
+
+        try (PreparedStatement pstmt = connection.prepareStatement(createFavoriteRecipesTable)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException("Creating FavoriteRecipes table failed.", e);
         }
     }
 
     private void createDaoImplementations() {
         ingredientDao = new DatabaseIngredientDao(connection);
         recipeDao = new DatabaseRecipeDao(connection, ingredientDao);
+        userDao = new DatabaseUserDao(connection);
     }
 
     public Connection getConnection() {
         return connection;
     }
 
+    /**
+     * Closes the database connection.
+     * 
+     * @throws DatabaseException if closing the connection fails.
+     */
     @Override
-    public void closeDataStore() {
+    public void closeDataStore() throws DatabaseException {
         try {
             if (connection != null) {
                 connection.close();
             }
         } catch (SQLException e) {
-            System.out.println("Closing database connection failed.");
-            e.printStackTrace();
+            throw new DatabaseException("Closing database connection failed.", e);
         }
     }
 }
