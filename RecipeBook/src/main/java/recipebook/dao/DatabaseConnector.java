5e784ad7546bf9bdb0bfff5309a2c611f4125b79
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:22:09 +0300

    Refactor and update javadoc

--- RecipeBook/src/main/java/recipebook/dao/DatabaseConnector.java
+++ RecipeBook/src/main/java/recipebook/dao/DatabaseConnector.java
@@ -9,15 +9,21 @@ import recipebook.dao.ingredientdao.DatabaseIngredientDao;
 import recipebook.dao.recipedao.DatabaseRecipeDao;
 import recipebook.dao.userdao.DatabaseUserDao;
 
+/**
+ * Handles the opening and closing the database connection and initializes the
+ * DAO implementations used in the application. Extends DataStoreConnector.
+ *
+ * @see recipebook.dao.DataStoreConnector
+ */
 public class DatabaseConnector extends DataStoreConnector {
 
     private Connection connection = null;
 
     /**
-     * Constructor
+     * Constructor.
      * 
-     * @param dataStoreLocation Path to the folder where the datastore file is
-     *                          located.
+     * @param dataStoreLocation Path to the folder where the database is located.
+     *
      */
     public DatabaseConnector(String dataStoreLocation) {
         this.dataStoreLocation = dataStoreLocation;
@@ -25,25 +31,26 @@ public class DatabaseConnector extends DataStoreConnector {
 
     /**
      * Creates the database directory if necessary, connects to the database file,
-     * creates the database tables and dao implementations.
+     * creates the database tables (if necessary), as well as the DAO
+     * implementations.
      */
     @Override
-    public void initializeDataStore() throws DatabaseException {
+    public void initializeDataStore() throws DataStoreException {
         createDirectoryIfNotExists();
         connectToDatabase();
         createTables();
         createDaoImplementations();
     }
 
-    private void connectToDatabase() throws DatabaseException {
+    private void connectToDatabase() throws DataStoreException {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:" + dataStoreLocation + "recipes.db");
         } catch (SQLException e) {
-            throw new DatabaseException("Connection to database failed.", e);
+            throw new DataStoreException("Connection to database failed.", e);
         }
     }
 
-    private void createTables() throws DatabaseException {
+    private void createTables() throws DataStoreException {
         createUsersTable();
         createIngredientsTable();
         createRecipesTable();
@@ -51,79 +58,75 @@ public class DatabaseConnector extends DataStoreConnector {
         createFavoriteRecipesTable();
     }
 
-    private void createRecipesTable() throws DatabaseException {
+    private void createRecipesTable() throws DataStoreException {
         String createRecipesTable = QueryBuilder.generateCreateRecipesTableQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(createRecipesTable)) {
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new DatabaseException("Creating Recipes table failed.", e);
+            throw new DataStoreException("Creating Recipes table failed.", e);
         }
     }
 
-    private void createIngredientsTable() throws DatabaseException {
+    private void createIngredientsTable() throws DataStoreException {
         String createIngredientsTable = QueryBuilder.generateCreateIngredientsTableQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(createIngredientsTable)) {
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new DatabaseException("Creating Ingredients table failed.", e);
+            throw new DataStoreException("Creating Ingredients table failed.", e);
         }
     }
 
-    private void createRecipesIngredientsTable() throws DatabaseException {
+    private void createRecipesIngredientsTable() throws DataStoreException {
         String createRecipesIngredientsTable = QueryBuilder.generateCreateRecipesIngredientsTableQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(createRecipesIngredientsTable)) {
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new DatabaseException("Creating RecipesIngredients table failed.", e);
+            throw new DataStoreException("Creating RecipesIngredients table failed.", e);
         }
     }
 
-    private void createUsersTable() throws DatabaseException {
+    private void createUsersTable() throws DataStoreException {
         String createUsersTable = QueryBuilder.generateCreateUsersTableQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(createUsersTable)) {
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new DatabaseException("Creating Users table failed.", e);
+            throw new DataStoreException("Creating Users table failed.", e);
         }
     }
 
-    private void createFavoriteRecipesTable() throws DatabaseException {
+    private void createFavoriteRecipesTable() throws DataStoreException {
         String createFavoriteRecipesTable = QueryBuilder.generateCreateFavoriteRecipesTablesQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(createFavoriteRecipesTable)) {
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new DatabaseException("Creating FavoriteRecipes table failed.", e);
+            throw new DataStoreException("Creating FavoriteRecipes table failed.", e);
         }
     }
 
     private void createDaoImplementations() {
         userDao = new DatabaseUserDao(connection);
-        ingredientDao = new DatabaseIngredientDao(connection, userDao);
-        recipeDao = new DatabaseRecipeDao(connection, ingredientDao);
-    }
-
-    public Connection getConnection() {
-        return connection;
+        ingredientDao = new DatabaseIngredientDao(connection);
+        recipeDao = new DatabaseRecipeDao(connection, ingredientDao, userDao);
     }
 
     /**
      * Closes the database connection.
-     * 
-     * @throws DatabaseException if closing the connection fails.
+     *
+     * @throws DataStoreException if closing the connection fails.
      */
     @Override
-    public void closeDataStore() throws DatabaseException {
+    public void closeDataStore() throws DataStoreException {
         try {
             if (connection != null) {
                 connection.close();
             }
         } catch (SQLException e) {
-            throw new DatabaseException("Closing database connection failed.", e);
+            throw new DataStoreException("Closing database connection failed.", e);
         }
     }
 }
