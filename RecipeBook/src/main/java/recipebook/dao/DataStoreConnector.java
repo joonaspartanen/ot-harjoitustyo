4abc89211f5da04adfa0ad170d750379e6c1e9ce
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:21:31 +0300

    Update javadoc

--- RecipeBook/src/main/java/recipebook/dao/DataStoreConnector.java
+++ RecipeBook/src/main/java/recipebook/dao/DataStoreConnector.java
@@ -5,13 +5,13 @@ import java.io.File;
 import recipebook.dao.ingredientdao.IngredientDao;
 import recipebook.dao.recipedao.RecipeDao;
 import recipebook.dao.userdao.UserDao;
+import recipebook.dao.userdao.UserNotFoundException;
 
 /**
- * Abstract class that offers protected variables and protected and public
+ * Abstract class that provides protected variables and protected and public
  * methods for connecting to a database or a file where the application data is
  * stored.
  */
-
 public abstract class DataStoreConnector {
 
     protected IngredientDao ingredientDao;
@@ -22,18 +22,18 @@ public abstract class DataStoreConnector {
     /**
      * Abstract method to initialize the data store. Different implementations for
      * database and file store.
-     * 
-     * @throws DatabaseException
+     *
+     * @throws DataStoreException, UserNotFoundException
      */
-    public abstract void initializeDataStore() throws DatabaseException;
+    public abstract void initializeDataStore() throws DataStoreException, UserNotFoundException;
 
     /**
      * Abstract method to close the data store. Different implementations for
      * database and file store.
-     * 
-     * @throws DatabaseException
+     *
+     * @throws DataStoreException
      */
-    public abstract void closeDataStore() throws DatabaseException;
+    public abstract void closeDataStore() throws DataStoreException;
 
     public IngredientDao getIngredientDao() {
         return ingredientDao;
@@ -47,6 +47,9 @@ public abstract class DataStoreConnector {
         return userDao;
     }
 
+    /**
+     * Creates the directory structure to the data store location if missing.
+     */
     protected void createDirectoryIfNotExists() {
         File directory = new File(dataStoreLocation);
         if (!directory.exists()) {
