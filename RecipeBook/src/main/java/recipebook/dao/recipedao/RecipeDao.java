6368110756539f49828a3f779c240e85b240fe78
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:28:29 +0300

    Update javadoc

--- RecipeBook/src/main/java/recipebook/dao/recipedao/RecipeDao.java
+++ RecipeBook/src/main/java/recipebook/dao/recipedao/RecipeDao.java
@@ -2,22 +2,88 @@ package recipebook.dao.recipedao;
 
 import java.util.List;
 
+import recipebook.dao.DataStoreException;
+import recipebook.dao.userdao.UserNotFoundException;
 import recipebook.domain.recipe.Recipe;
 
 /**
- * Dao interface for handling Recipes-related data.
+ * DAO interface for handling Recipes-related data.
  */
 public interface RecipeDao {
 
-    Recipe create(Recipe recipe);
+    /**
+     * Stores a recipe to the data store.
+     *
+     * @param recipe Recipe to be stored.
+     * @return The stored recipe.
+     * @throws DataStoreException
+     */
+    Recipe create(Recipe recipe) throws DataStoreException;
 
-    List<Recipe> getAll();
+    /**
+     * Fetches all recipes from the data store.
+     *
+     * @return List of recipes.
+     * @throws DataStoreException
+     * @throws UserNotFoundException
+     */
+    List<Recipe> getAll() throws DataStoreException, UserNotFoundException;
 
-    List<Recipe> getByName(String name);
+    /**
+     * Fetches recipes whose name contain the search term passed in as a parameter.
+     *
+     * @param name Recipe name used as a search term.
+     * @return List of recipes or an empty list if no results found.
+     * @throws DataStoreException
+     * @throws UserNotFoundException
+     */
+    List<Recipe> getByName(String name) throws DataStoreException, UserNotFoundException;
 
-    Recipe getById(int id);
+    /**
+     * Fetches the recipes whose id matches the search term passed in as a
+     * parameter. Single result expected.
+     *
+     * @param id The recipe id used as a search term.
+     * @return The matching recipe.
+     * @throws DataStoreException    if no results found.
+     * @throws UserNotFoundException
+     */
+    Recipe getById(int id) throws DataStoreException, UserNotFoundException;
 
-    List<Recipe> getByIngredient(String name);
+    /**
+     * Fetches recipes that contain certain ingredient.
+     *
+     * @param name Name of the ingredient used as a search term.
+     * @return List of recipes or an empty list if no results found.
+     * @throws DataStoreException
+     * @throws UserNotFoundException
+     */
+    List<Recipe> getByIngredient(String name) throws DataStoreException, UserNotFoundException;
 
-    void delete(Recipe recipe);
+    /**
+     * Deletes a recipe from the data store.
+     *
+     * @param recipe The recipe to be deleted.
+     * @throws DataStoreException
+     */
+    void delete(Recipe recipe) throws DataStoreException;
+
+    /**
+     * Add a recipe to user favorites.
+     *
+     * @param userId Id of the user who is adding the recipe to favorites.
+     * @param recipe The recipe to be added to favorites.
+     * @throws DataStoreException
+     */
+    void saveRecipeToFavorites(int userId, Recipe recipe) throws DataStoreException;
+
+    /**
+     * Fetches a list of user's favorite recipes.
+     *
+     * @param userId Id of the user whose favorite recipes are being fetched.
+     * @return List of recipes or an empty list if no results found.
+     * @throws DataStoreException
+     * @throws UserNotFoundException
+     */
+    List<Recipe> getFavoriteRecipes(int userId) throws DataStoreException, UserNotFoundException;
 }
