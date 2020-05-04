5f0c0e0fdf2b0f7c8bdab19c32119e0e1ab759f9
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:33:30 +0300

    Update javadoc and error handling

--- RecipeBook/src/main/java/recipebook/domain/recipe/RecipeService.java
+++ RecipeBook/src/main/java/recipebook/domain/recipe/RecipeService.java
@@ -1,4 +1,3 @@
-
 package recipebook.domain.recipe;
 
 import recipebook.domain.ingredient.Ingredient;
@@ -8,7 +7,9 @@ import recipebook.domain.user.UserService;
 import java.util.List;
 import java.util.Map;
 
+import recipebook.dao.DataStoreException;
 import recipebook.dao.recipedao.RecipeDao;
+import recipebook.dao.userdao.UserNotFoundException;
 
 /**
  * Contains the business logic for handling Recipes objects. Dependencies are
@@ -26,24 +27,26 @@ public class RecipeService {
 
     /**
      * Method to fetch all recipes stored in the current data store.
-     * 
+     *
      * @return List of recipes.
+     * @throws DataStoreException
      */
-    public List<Recipe> listAll() {
+    public List<Recipe> listAll() throws DataStoreException, UserNotFoundException {
         return recipeDao.getAll();
     }
 
     /**
-     * Method to create a new recipe and store using the dao dependency. Parameters
-     * self-explanatory. Gets the recipe author by the UserService.
-     * 
+     * Method to create a new recipe and store using the DAO dependency. Parameters
+     * self-explanatory. Fetches the recipe author from the UserService.
+     *
      * @param name
      * @param ingredients
      * @param time
      * @param instructions
      * @return The newly created Recipe object.
      */
-    public Recipe createRecipe(String name, Map<Ingredient, Integer> ingredients, int time, String instructions) {
+    public Recipe createRecipe(String name, Map<Ingredient, Integer> ingredients, int time, String instructions)
+            throws DataStoreException {
         User currentUser = userService.getCurrentUser();
         Recipe recipe = new Recipe(name, ingredients, time, instructions, currentUser);
         return recipeDao.create(recipe);
@@ -51,23 +54,25 @@ public class RecipeService {
 
     /**
      * Method to fetch all recipes that contain certain ingredient.
-     * 
+     *
      * @param name Name of the ingredient that matching recipes should contain.
      * @return List of recipes.
+     * @throws DataStoreException
      */
-    public List<Recipe> findByIngredient(String name) {
+    public List<Recipe> findByIngredient(String name) throws DataStoreException, UserNotFoundException {
         return recipeDao.getByIngredient(name);
     }
 
     /**
      * Method to delete a recipe from the database or file. Checks if the recipe
      * exists and if the current user is its author.
-     * 
+     *
      * @param recipeId Id of the recipe to delete.
      * @return true if the recipe is deleted successfully; false if the operation
      *         fails or is denied.
+     * @throws DataStoreException
      */
-    public boolean deleteRecipeById(int recipeId) {
+    public boolean deleteRecipeById(int recipeId) throws DataStoreException, UserNotFoundException {
         Recipe recipe = recipeDao.getById(recipeId);
         if (recipe == null) {
             return false;
@@ -81,6 +86,39 @@ public class RecipeService {
         }
 
         return false;
+    }
+
+    /**
+     * Adds a recipe to the favorite recipes of the current user.
+     * 
+     * @param recipe The recipe to be added to favorites.
+     * @throws DataStoreException
+     * @throws UserNotFoundException
+     */
+    public void addRecipeToFavorites(Recipe recipe) throws DataStoreException, UserNotFoundException {
+        User currentUser = userService.getCurrentUser();
+        int userId = currentUser.getId();
+        if (recipeNotInFavorites(recipe)) {
+            recipeDao.saveRecipeToFavorites(userId, recipe);
+        }
+    }
+
+    private boolean recipeNotInFavorites(Recipe recipe) throws DataStoreException, UserNotFoundException {
+        return !getFavoriteRecipes().contains(recipe);
+    }
+
+    /**
+     * Fetches the current user's favorite recipes.
+     * 
+     * @return List of recipes or an empty list if no results found.
+     * @throws DataStoreException
+     * @throws UserNotFoundException
+     */
+    public List<Recipe> getFavoriteRecipes() throws DataStoreException, UserNotFoundException {
+        User currentUser = userService.getCurrentUser();
+        int userId = currentUser.getId();
+        List<Recipe> favoriteRecipes = recipeDao.getFavoriteRecipes(userId);
+        return favoriteRecipes;
     }
 
     private boolean userAllowedToDeleteRecipe(User user, Recipe recipe) {
