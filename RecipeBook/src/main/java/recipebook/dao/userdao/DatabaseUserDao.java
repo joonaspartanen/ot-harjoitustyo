54fca5a3cd8771af068139244b2c6c1d0790abc6
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:31:04 +0300

    Update javadoc and error handling

--- RecipeBook/src/main/java/recipebook/dao/userdao/DatabaseUserDao.java
+++ RecipeBook/src/main/java/recipebook/dao/userdao/DatabaseUserDao.java
@@ -5,25 +5,42 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import recipebook.dao.DaoHelper;
+import recipebook.dao.DataStoreException;
+import recipebook.dao.IdExtractor;
 import recipebook.dao.QueryBuilder;
 import recipebook.dao.ResultSetMapper;
 import recipebook.domain.user.User;
 
+/**
+ * UserDao implementation for storing data into a database.
+ *
+ */
 public class DatabaseUserDao implements UserDao {
 
     private Connection connection;
-    private DaoHelper daoHelper;
+    private IdExtractor idExtractor;
     private ResultSetMapper mapper;
 
+    /**
+     * Constructor.
+     * 
+     * @param connection Connection to the database.
+     */
     public DatabaseUserDao(Connection connection) {
         this.connection = connection;
-        daoHelper = new DaoHelper();
+        idExtractor = new IdExtractor();
         mapper = new ResultSetMapper(this);
     }
 
+    /**
+     * Stores a new user.
+     * 
+     * @param user User to be stored.
+     * @return The stored user with corresponding id.
+     * @throws DataStoreException
+     */
     @Override
-    public User create(User user) {
+    public User create(User user) throws DataStoreException {
         String createUserQuery = QueryBuilder.generateInsertUserQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(createUserQuery, Statement.RETURN_GENERATED_KEYS)) {
@@ -31,15 +48,23 @@ public class DatabaseUserDao implements UserDao {
             pstmt.setString(1, user.getUsername());
             pstmt.executeUpdate();
 
-            user.setId(daoHelper.getCreatedItemId(pstmt));
+            user.setId(idExtractor.getCreatedItemIdFromDatabase(pstmt));
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            throw new DataStoreException("Storing the user data into the database failed.", ex);
         }
 
         return user;
     }
 
+    /**
+     * Fetches the user whose username matches the search term passed in as a
+     * parameter. Single result expected.
+     *
+     * @param username Username used as a search term.
+     * @return The matching user.
+     * @throws UserNotFoundException if no matches found.
+     */
     @Override
     public User getByUsername(String username) throws UserNotFoundException {
         User user = null;
@@ -47,7 +72,6 @@ public class DatabaseUserDao implements UserDao {
 
         try (PreparedStatement pstmt = connection.prepareStatement(getUserByUsernameQuery)) {
             pstmt.setString(1, username);
-            pstmt.executeQuery();
 
             user = mapper.extractSingleUser(pstmt);
 
@@ -62,23 +86,27 @@ public class DatabaseUserDao implements UserDao {
         return user;
     }
 
+    /**
+     * Fetches the user whose id matches the search term passed in as a parameter.
+     *
+     * @param id Id used as a search term.
+     * @return The matching user.
+     * @throws UserNotFoundException
+     */
     @Override
-    public User getById(int id) {
+    public User getById(int id) throws UserNotFoundException {
         User user = null;
 
         String getUserByIdQuery = QueryBuilder.generateSelectUserByIdQuery();
 
         try (PreparedStatement pstmt = connection.prepareStatement(getUserByIdQuery)) {
             pstmt.setInt(1, id);
-            pstmt.executeQuery();
-
             user = mapper.extractSingleUser(pstmt);
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException ex) {
+            throw new UserNotFoundException("Fetching the user from the database failed.", ex);
         }
 
         return user;
     }
-
 }
