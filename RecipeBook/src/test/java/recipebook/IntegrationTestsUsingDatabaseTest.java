f8b5544acbe3d9e53f3c568d41a6819452af63eb
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:35:34 +0300

    Create integration tests

---
+++ RecipeBook/src/test/java/recipebook/IntegrationTestsUsingDatabaseTest.java
@@ -0,0 +1,111 @@
+package recipebook;
+
+import java.util.List;
+import java.util.Map;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import recipebook.dao.DataStoreConnector;
+import recipebook.dao.DatabaseConnector;
+import recipebook.dao.DataStoreException;
+import recipebook.dao.ingredientdao.IngredientDao;
+import recipebook.dao.recipedao.RecipeDao;
+import recipebook.dao.userdao.UserDao;
+import recipebook.dao.userdao.UserNotFoundException;
+import recipebook.domain.ingredient.Ingredient;
+import recipebook.domain.recipe.Recipe;
+import recipebook.domain.recipe.RecipeService;
+import recipebook.domain.user.BadUsernameException;
+import recipebook.domain.user.UserService;
+
+public class IntegrationTestsUsingDatabaseTest {
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    private DataStoreConnector connector;
+    private UserService userService;
+    private RecipeService recipeService;
+    private TestHelper helper;
+    private RecipeDao recipeDao;
+
+    @Before
+    public void setUp() throws DataStoreException, UserNotFoundException {
+        connector = new DatabaseConnector(testFolder.getRoot().toString() + "/");
+        connector.initializeDataStore();
+        UserDao userDao = connector.getUserDao();
+        IngredientDao ingredientDao = connector.getIngredientDao();
+        recipeDao = connector.getRecipeDao();
+        userService = new UserService(userDao);
+        recipeService = new RecipeService(recipeDao, userService);
+        helper = new TestHelper(ingredientDao, userDao);
+    }
+
+    @Test
+    public void userCanAddRecipes() throws BadUsernameException, UserNotFoundException, DataStoreException {
+        createUserAndLogin("Integration tester");
+        Recipe salmonSoup = addTestRecipeWithName("Salmon soup");
+
+        List<Recipe> allRecipes = recipeService.listAll();
+        assertThat(allRecipes, hasItem(salmonSoup));
+
+        Recipe foundRecipe = allRecipes.stream().filter(r -> r.getName().equals("Salmon soup")).findFirst()
+                .orElse(null);
+        int userId = userService.getCurrentUser().getId();
+        assertThat(foundRecipe.getAuthorId(), is(userId));
+    }
+
+    @Test
+    public void onlyRecipeAuthorCanDeleteRecipe()
+            throws BadUsernameException, UserNotFoundException, DataStoreException {
+        createUserAndLogin("Tester 1");
+        Recipe salmonSoup = addTestRecipeWithName("Salmon soup");
+        userService.logout();
+
+        createUserAndLogin("Tester 2");
+        recipeService.deleteRecipeById(salmonSoup.getId());
+        List<Recipe> allRecipes = recipeService.listAll();
+        assertThat(allRecipes, hasItem(salmonSoup));
+        userService.logout();
+
+        userService.login("Tester 1");
+        recipeService.deleteRecipeById(salmonSoup.getId());
+        allRecipes = recipeService.listAll();
+
+        assertThat(allRecipes, not(hasItem(salmonSoup)));
+    }
+
+    @Test
+    public void userCanAddRecipesToFavorites() throws BadUsernameException, UserNotFoundException, DataStoreException {
+        createUserAndLogin("Tester 1");
+        Recipe salmonSoup = addTestRecipeWithName("Salmon soup");
+        userService.logout();
+
+        createUserAndLogin("Tester 2");
+        Recipe chickenSoup = addTestRecipeWithName("Chicken soup");
+        recipeService.addRecipeToFavorites(salmonSoup);
+
+        List<Recipe> favoriteRecipes = recipeService.getFavoriteRecipes();
+
+        assertThat(favoriteRecipes, hasItem(salmonSoup));
+        assertThat(favoriteRecipes, hasItem(chickenSoup));
+    }
+
+    private void createUserAndLogin(String username)
+            throws BadUsernameException, UserNotFoundException, DataStoreException {
+        userService.createUser(username);
+        userService.login(username);
+    }
+
+    private Recipe addTestRecipeWithName(String recipeName) throws DataStoreException {
+        Map<Ingredient, Integer> ingredients = helper.createTestIngredientListWithNames("salmon", "milk", "butter",
+                "potato");
+        return recipeService.createRecipe(recipeName, ingredients, 40, "Boil until done.");
+    }
+
+}
