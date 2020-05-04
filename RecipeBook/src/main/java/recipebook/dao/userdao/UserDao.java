5a6adcd4faeb571133da0eed22c7acafb6967242
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:31:32 +0300

    Update javadoc

--- RecipeBook/src/main/java/recipebook/dao/userdao/UserDao.java
+++ RecipeBook/src/main/java/recipebook/dao/userdao/UserDao.java
@@ -1,16 +1,40 @@
 package recipebook.dao.userdao;
 
+import recipebook.dao.DataStoreException;
 import recipebook.domain.user.User;
 
 /**
- * Dao interface for handling User-related data. 
+ * DAO interface for handling user-related data.
  */
 public interface UserDao {
 
-    User create(User user);
+    /**
+     * Stores an user to the data store.
+     *
+     * @param user User to be stored.
+     * @return The stored user with corresponding id.
+     * @throws DataStoreException
+     */
+    User create(User user) throws DataStoreException;
 
+    /**
+     * Fetches the user whose username matches the search term passed in as a
+     * parameter. Single result expected.
+     *
+     * @param username Username used as a search term.
+     * @return The matching user.
+     * @throws UserNotFoundException if no matches found.
+     */
     User getByUsername(String username) throws UserNotFoundException;
 
-    User getById(int id);
+    /**
+     * Fetches the user whose id matches the search term passed in as a parameter.
+     *
+     * @param id Id used as a search term.
+     * @return The matching user.
+     * @throws UserNotFoundException
+     *
+     */
+    User getById(int id) throws UserNotFoundException;
 
 }
