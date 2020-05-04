b80a4cf381ea6374b16c76c76bd85023c97d7a2d
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:22:47 +0300

    Rename for clarity

--- RecipeBook/src/main/java/recipebook/dao/IdExtractor.java
+++ RecipeBook/src/main/java/recipebook/dao/IdExtractor.java
@@ -3,24 +3,36 @@ package recipebook.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
- * Provides helper methods common to all dao classes.
+ * Provides helper methods common to all DAO classes.
  */
-public class DaoHelper {
+public class IdExtractor {
 
     /**
      * Returns the id of the item inserted into the database.
-     * @param pstmt Prepared Statement to insert the item. 
+     *
+     * @param pstmt Prepared Statement to insert the item.
      * @return Id of the inserted item.
-     * @throws SQLException 
+     * @throws SQLException
      */
-    public int getCreatedItemId(PreparedStatement pstmt) throws SQLException {
+    public int getCreatedItemIdFromDatabase(PreparedStatement pstmt) throws SQLException {
         int id = 0;
         ResultSet resultSet = pstmt.getGeneratedKeys();
         if (resultSet.next()) {
             id = resultSet.getInt(1);
         }
         return id;
+    }
+
+    /**
+     * Returns the id of the item inserted into a list.
+     *
+     * @param items List of items the created item was inserted into.
+     * @return Id of the inserted item.
+     */
+    public int getCreatedItemIdFromList(List<? extends Object> items) {
+        return items.size() + 1;
     }
 }
