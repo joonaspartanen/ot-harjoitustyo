542ded3d8bcbaa1eca5907d5a917dad477c7a521
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:22:30 +0300

    Refactor and update javadoc

--- RecipeBook/src/main/java/recipebook/dao/FileConnector.java
+++ RecipeBook/src/main/java/recipebook/dao/FileConnector.java
@@ -2,17 +2,27 @@ package recipebook.dao;
 
 import recipebook.dao.ingredientdao.FileIngredientDao;
 import recipebook.dao.recipedao.FileRecipeDao;
+import recipebook.dao.userdao.FileUserDao;
+import recipebook.dao.userdao.UserNotFoundException;
 
+/**
+ * Handles the connection to the text files used as a data store and initializes
+ * the DAO implementations used in the application.
+ *
+ * @see recipebook.dao.DataStoreConnector
+ */
 public class FileConnector extends DataStoreConnector {
 
     private String ingredientsFile;
     private String recipesFile;
     private String recipesIngredientsFile;
+    private String userFile;
+    private String favoriteRecipesFile;
 
     /**
      * Constructor
-     * 
-     * @param dataStoreLocation Path to the folder where the datastore file is
+     *
+     * @param dataStoreLocation Path to the folder where the data store file is
      *                          located.
      */
     public FileConnector(String dataStoreLocation) {
@@ -20,21 +30,26 @@ public class FileConnector extends DataStoreConnector {
     }
 
     /**
-     * Creates the datastore directory if necessary, concatenates the path to
-     * datastore file names and creates the dao implementations.
+     * Creates the data store directory if necessary, concatenates the path to data
+     * store file names and creates the DAO implementations.
+     * 
+     * @throws recipebook.dao.userdao.UserNotFoundException
      */
     @Override
-    public void initializeDataStore() {
+    public void initializeDataStore() throws DataStoreException, UserNotFoundException {
         createDirectoryIfNotExists();
         initializeFileNames();
         ingredientDao = new FileIngredientDao(ingredientsFile);
-        recipeDao = new FileRecipeDao(ingredientDao, recipesFile, recipesIngredientsFile);
+        userDao = new FileUserDao(userFile);
+        recipeDao = new FileRecipeDao(ingredientDao, userDao, recipesFile, recipesIngredientsFile, favoriteRecipesFile);
     }
 
     private void initializeFileNames() {
         ingredientsFile = dataStoreLocation + "ingredients.txt";
         recipesFile = dataStoreLocation + "recipes.txt";
         recipesIngredientsFile = dataStoreLocation + "recipesIngredients.txt";
+        userFile = dataStoreLocation + "users.txt";
+        favoriteRecipesFile = dataStoreLocation + "favoriteRecipes.txt";
     }
 
     /**
