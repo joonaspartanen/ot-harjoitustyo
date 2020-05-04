a6c460212a52789b0fcb73b680b4f3a4e454c91f
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:21:57 +0300

    Update javadoc and add constructor

--- RecipeBook/src/main/java/recipebook/dao/DataStoreException.java
+++ RecipeBook/src/main/java/recipebook/dao/DataStoreException.java
@@ -1,23 +1,33 @@
 package recipebook.dao;
 
 /**
- * SQLExpections are wrapped into the DatabaseException. DatabaseExceptions are
- * caught in the Ui and used to show descriptive error messages.
+ * SQLExpections are wrapped into the DataStoreException. DataStoreExceptions
+ * are caught in the UI and used to show descriptive error messages.
  */
-public class DatabaseException extends Exception {
+public class DataStoreException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     /**
      * Constructor
-     * 
-     * @param errorMessage      Descriptive error message
+     *
+     * @param errorMessage Descriptive error message
      * @param originalException The original exception is wrapped into
-     *                          DatabaseException in order to conserve the original
-     *                          stack trace for debugging purposes.
+     * DataStoreException in order to conserve the original stack trace for
+     * debugging purposes.
      */
-    public DatabaseException(String errorMessage, Throwable originalException) {
+    public DataStoreException(String errorMessage, Throwable originalException) {
         super(errorMessage, originalException);
+    }
+
+    /**
+     * Constructor for cases where no other exception is wrapped into the
+     * DataStoreException.
+     *
+     * @param errorMessage Descriptive error message.
+     */
+    public DataStoreException(String errorMessage) {
+        super(errorMessage);
     }
 
 }
