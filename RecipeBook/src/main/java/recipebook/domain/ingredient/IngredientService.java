e7f7c3a27544903a458cd5315f46ae33b4635927
Joonas Partanen <partanen.joonas@gmail.com>
2020-05-04 08:32:39 +0300

    Update javadoc and error handling

--- RecipeBook/src/main/java/recipebook/domain/ingredient/IngredientService.java
+++ RecipeBook/src/main/java/recipebook/domain/ingredient/IngredientService.java
@@ -2,6 +2,7 @@ package recipebook.domain.ingredient;
 
 import java.util.List;
 
+import recipebook.dao.DataStoreException;
 import recipebook.dao.ingredientdao.IngredientDao;
 
 /**
@@ -10,34 +11,36 @@ import recipebook.dao.ingredientdao.IngredientDao;
  */
 public class IngredientService {
 
-    private IngredientDao ingDao;
+    private IngredientDao ingredientDao;
 
     public IngredientService(IngredientDao ingDao) {
-        this.ingDao = ingDao;
+        this.ingredientDao = ingDao;
     }
 
     /**
      * Method to fetch all ingredients stored in the current data store.
      * 
      * @return List of ingredients.
+     * @throws DataStoreException
      */
-    public List<Ingredient> listAll() {
-        return ingDao.getAll();
+    public List<Ingredient> listAll() throws DataStoreException {
+        return ingredientDao.getAll();
     }
 
     /**
      * Method to create a new ingredient and store it by the dao dependency. Checks
      * if the ingredient with same name and unit exists to avoid duplicate items.
      * 
-     * @param name
-     * @param unit
+     * @param name Ingredient name.
+     * @param unit Ingredient unit.
      * @return Returns the newly created Ingredient object or, if existent, an
      *         ingredient with the same name and unit.
+     * @throws DataStoreException
      */
-    public Ingredient createIngredient(String name, String unit) {
-        Ingredient foundIngredient = ingDao.getByNameAndUnit(formatName(name), unit);
+    public Ingredient createIngredient(String name, String unit) throws DataStoreException {
+        Ingredient foundIngredient = ingredientDao.getByNameAndUnit(formatName(name), unit);
         if (foundIngredient == null) {
-            return ingDao.create(new Ingredient(formatName(name), unit));
+            return ingredientDao.create(new Ingredient(formatName(name), unit));
         }
         return foundIngredient;
     }
@@ -48,11 +51,12 @@ public class IngredientService {
      * @see recipebook.domain.ingredient.IngredientService#createIngredient(String,
      *      String)
      * 
-     * @param name
+     * @param name Ingredient name.
      * @return Returns the newly created Ingredient object or, if existent, an
      *         ingredient with the same name and unit.
+     * @throws DataStoreException
      */
-    public Ingredient createIngredient(String name) {
+    public Ingredient createIngredient(String name) throws DataStoreException {
         return createIngredient(name, "g");
     }
 
@@ -61,24 +65,26 @@ public class IngredientService {
      * ingredient names are case-insensitive and the leading or trailing spaces are
      * trimmed.
      * 
-     * @param name
+     * @param name Ingredient name.
      * @return List of matching ingredients or an empty list if no matches are
      *         found.
+     * @throws DataStoreException
      */
-    public List<Ingredient> findByName(String name) {
-        return ingDao.getByName(formatName(name));
+    public List<Ingredient> findByName(String name) throws DataStoreException {
+        return ingredientDao.getByName(formatName(name));
     }
 
     /**
      * Method to fetch an ingredient with certain name and unit. Only one match is
      * expected as duplicates are not stored.
      * 
-     * @param name
-     * @param unit
+     * @param name Ingredient name.
+     * @param unit Ingredient unit.
      * @return A matching Ingredient object or null if no matches are found.
+     * @throws DataStoreException
      */
-    public Ingredient findByNameAndUnit(String name, String unit) {
-        return ingDao.getByNameAndUnit(formatName(name), unit);
+    public Ingredient findByNameAndUnit(String name, String unit) throws DataStoreException {
+        return ingredientDao.getByNameAndUnit(formatName(name), unit);
     }
 
     private String formatName(String name) {
